415c4432291c94224591d6dd98dfc14db49f44cc
PaddyMc <paddymchale@hotmail.com>
2020-10-26 18:20:40 +0000

    feat(bdd): add create issuer functionality

--- test/e2e/issuers/go.mod
+++ test/e2e/issuers/go.mod
@@ -2,4 +2,7 @@ module issuers
 
 go 1.15
 
-require github.com/cucumber/godog v0.10.0
+require (
+	github.com/cucumber/godog v0.10.0
+	github.com/hashicorp/go-uuid v1.0.1 // indirect
+)
--- test/e2e/issuers/issuers_test.go
+++ test/e2e/issuers/issuers_test.go
@@ -1,11 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	osexec "os/exec"
+	"os"
+	"os/exec"
+	"testing"
 
 	"github.com/cucumber/godog"
+	"github.com/cucumber/godog/colors"
 )
+
+var opts = godog.Options{Output: colors.Colored(os.Stdout)}
+
+func iAmAnAdmin(arg1 string) error {
+	output, err := exec.Command("poacli", "keys", "show", arg1, "-a").Output()
+	if len(output) == 0 {
+		return fmt.Errorf("admin key is unavailable")
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func iCreateAnIssuerWithTokens(arg1 string, arg2 string, arg3 string) error {
+	output, err := exec.Command("poacli", "keys", "show", arg1, "-a").Output()
+	if err != nil {
+		return err
+	}
+	s := string(output)
+	issuerAddress := s[:len(s)-1]
+	txOutput, err := exec.Command("poacli", "tx", "issuer", "create-issuer", arg1, issuerAddress, arg3, arg2, "--trust-node", "-y", "--from", "validator", "--chain-id", "cash", "--home", "../../../build/.poad").Output()
+	fmt.Println(string(txOutput))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func iCreateAndIssuerAccountWithName(arg1 string) error {
+	_, err := exec.Command("poacli", "keys", "add", arg1).Output()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	ctx.Step(`^I am an admin called "([^"]*)"$`, iAmAnAdmin)
+	ctx.Step(`^I create an account with name "([^"]*)"$`, iCreateAndIssuerAccountWithName)
+	ctx.Step(`^I create an issuer "([^"]*)" with "([^"]*)" "([^"]*)"$`, iCreateAnIssuerWithTokens)
+
+	// TODO: add in steps
+	//	ctx.Step(`^I am an issuer$`, iAmAnIssuer)
+	//	ctx.Step(`^my account has (\d+) tokens$`, myAccountHasTokens)
+	//	ctx.Step(`^I create (\d+) tokens$`, iCreateTokens)
+	//	ctx.Step(`^my account will have (\d+) tokens$`, myAccountWillHaveTokens)
+}
 
 func iAmAnIssuer() error {
 	return godog.ErrPending
@@ -23,23 +75,23 @@ func myAccountWillHaveTokens(arg1 int) error {
 	return godog.ErrPending
 }
 
-func InitializeScenario(ctx *godog.ScenarioContext) {
-	ctx.Step(`^I am an issuer$`, iAmAnIssuer)
-	ctx.Step(`^my account has (\d+) tokens$`, myAccountHasTokens)
-	ctx.Step(`^I create (\d+) tokens$`, iCreateTokens)
-	ctx.Step(`^my account will have (\d+) tokens$`, myAccountWillHaveTokens)
+func init() {
+	godog.BindFlags("godog.", flag.CommandLine, &opts)
 }
 
-// execute executes a shell command.
-func exec(args ...string) error {
-	cmd := osexec.Command(args[0], args[1:]...)
-	out, err := cmd.CombinedOutput()
-	switch err := err.(type) {
-	case nil:
-		return nil
-	case *osexec.ExitError:
-		return fmt.Errorf("failed to run %q:\n%v", args, string(out))
-	default:
-		return err
+func TestMain(m *testing.M) {
+	flag.Parse()
+	opts.Paths = flag.Args()
+
+	status := godog.TestSuite{
+		Name:                "godogs",
+		ScenarioInitializer: InitializeScenario,
+		Options:             &opts,
+	}.Run()
+
+	if st := m.Run(); st > status {
+		status = st
 	}
+
+	os.Exit(status)
 }
